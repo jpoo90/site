f76c90aa6fdbfbe16c80416a3600e7f3587ba81e
JP Osorio <jpoo90@gmail.com>
2016-04-19 09:54:21 -0400

    Added method to send media in tweets.

--- other/utils/twitter.js
+++ other/utils/twitter.js
@@ -1,4 +1,5 @@
 import Twitter from 'twitter'
+import fs from 'fs'
 
 const TW = new Twitter(getTwitterConfig())
 export {getProfileImageURL, sendTweet}
@@ -16,9 +17,35 @@ function getProfileImageURL(handle) {
   })
 }
 
-function sendTweet(message) {
+function sendTweet(message, imgPath) {
+  prepareTweet(message, imgPath)
+    .then(postTweet)
+    .catch(logError)
+}
+
+
+function prepareTweet(message, imgPath) {
+
+  const tweetObj = {
+    status: message,
+  }
+
+  return new Promise((resolve) => {
+    if (imgPath) {
+      loadTweetImage(imgPath).then((mediaId) => {
+        tweetObj.media_ids = mediaId // eslint-disable-line camelcase
+        resolve(tweetObj)
+      })
+    } else {
+      resolve(tweetObj)
+    }
+  })
+}
+
+
+function postTweet(data) {
   return new Promise((resolve, reject) => {
-    TW.post('statuses/update', {status: message}, (error, tweet) => {
+    TW.post('statuses/update', data, (error, tweet) => {
       if (!error) {
         resolve(`Tweet sent ${tweet}`)
       } else {
@@ -28,10 +55,29 @@ function sendTweet(message) {
   })
 }
 
+function loadTweetImage(imgPath) {
+  const img = fs.readFileSync(imgPath)
+
+  return new Promise((resolve, reject) => {
+    TW.post('media/upload', {media: img}, (error, media) => {
+      if (!error) {
+        resolve(media.media_id_string) // eslint-disable-line camelcase
+      } else {
+        reject(error)
+      }
+    })
+  })
+
+}
+
 function getTwitterConfig() {
   try {
     return require('./twitter.api.ignored.json')
   } catch (e) {
     throw new Error('you must provide Twitter API info with a twitter.api.ignored.json')
   }
+}
+
+function logError(error) {
+  console.error('There was an error sending the tweet', error)
 }
